d673b400264af654842a73be024cffd41ed68f85
Filip Baotić <filipbaotic@outlook.com>
2021-01-18 02:17:08 +0100

    Update cart.js

--- cart.js
+++ cart.js
@@ -4,4 +4,32 @@ $(document).ready(function() {
         $(".cart").toggleClass("hidden");
         $(".view-cart").toggleClass("active");
     });
+
+    var count = 0;
+    $('.down').click(function() {
+        if (count < 2) {
+            count += 1;
+            $('.list').css('margin-top', '-=90px');
+        }
+    });
+    $('.up').click(function() {
+        if (count != 0) {
+            count -= 1;
+            $('.list').css('margin-top', '+=90px');
+        }
+    });
+    $('.up, .down').click(function() {
+        if (count < 2) {
+            $(".down").removeClass("disabled");
+        }
+        if (count == 2) {
+            $(".down").addClass("disabled");
+        }
+        if (count == 0) {
+            $(".up").addClass("disabled");
+        }
+        if (count != 0) {
+            $(".up").removeClass("disabled");
+        }
+    });
 });
